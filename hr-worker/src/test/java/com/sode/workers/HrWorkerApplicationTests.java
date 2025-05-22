cbd60304066ae030adf0716a83b2f3cc50a7b7a9
liam <iliamftw2013@gmail.com>
2025-05-22 14:52:59 -0300

    init: repo

---
+++ hr-worker/src/test/java/com/sode/workers/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sode.workers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
