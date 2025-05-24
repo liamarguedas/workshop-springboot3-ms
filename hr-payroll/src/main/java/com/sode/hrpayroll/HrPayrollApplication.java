87c5b0a516baa8fb7501cfd30495824dc039ebfe
liam <iliamftw2013@gmail.com>
2025-05-24 07:45:13 -0300

    add: hystrix tolerance

--- hr-payroll/src/main/java/com/sode/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/sode/hrpayroll/HrPayrollApplication.java
@@ -1,13 +1,14 @@
 package com.sode.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@RibbonClient(name = "hr-worker")
+@EnableCircuitBreaker
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
--- hr-payroll/src/main/java/com/sode/hrpayroll/resource/PaymentResource.java
+++ hr-payroll/src/main/java/com/sode/hrpayroll/resource/PaymentResource.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sode.hrpayroll.HrPayrollApplication;
 import com.sode.hrpayroll.entities.Payment;
 import com.sode.hrpayroll.services.PaymentService;
@@ -14,26 +16,25 @@ import com.sode.hrpayroll.services.PaymentService;
 @RequestMapping(value="/payments")
 public class PaymentResource {
 
-    private final HrPayrollApplication hrPayrollApplication;
-	
-	
 	@Autowired
 	private PaymentService service;
 
-
-    PaymentResource(HrPayrollApplication hrPayrollApplication) {
-        this.hrPayrollApplication = hrPayrollApplication;
-    }
 	
-	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value="/{id}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable long id, @PathVariable int days){
 		
 		Payment p = service.getPayment(id, days);
 		
 		return ResponseEntity.ok().body(p);
-		
-		
 	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(long id, int days){
+		
+		Payment p = new Payment("null", 0., days);
+		
+		return ResponseEntity.ok().body(p);
+	}
+	
 
 }
