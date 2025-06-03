4b358082b9209e548edbddd6a38a01c6941ef65e
liam <iliamftw2013@gmail.com>
2025-06-03 10:30:29 -0300

    add: oauth

---
+++ hr-oauth/src/main/java/com/sode/hroauth/services/UserService.java
@@ -0,0 +1,31 @@
+package com.sode.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sode.hroauth.entities.User;
+import com.sode.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+	
+	Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		
+		User u = userFeignClient.findByEmail(email).getBody();
+		
+		if ( u == null ) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		
+		logger.info("Email found: " + email);
+		return u;
+	}
+}
