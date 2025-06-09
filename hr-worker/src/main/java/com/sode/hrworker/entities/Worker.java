d98b7a63b6f7f529af782f260ea1ad4971f093fa
liam <iliamftw2013@gmail.com>
2025-06-09 18:22:06 -0300

    add: docker

--- hr-worker/src/main/java/com/sode/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/com/sode/hrworker/entities/Worker.java
@@ -1,7 +1,6 @@
 package com.sode.hrworker.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,20 +11,19 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_worker")
 public class Worker implements Serializable {
-	
-	
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
 	private String name;
 	private Double dailyIncome;
 	
-	public Worker() {}
+	public Worker() {
+	}
 
 	public Worker(Long id, String name, Double dailyIncome) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.dailyIncome = dailyIncome;
@@ -57,7 +55,10 @@ public class Worker implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -69,11 +70,11 @@ public class Worker implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Worker other = (Worker) obj;
-		return Objects.equals(id, other.id);
-	}
-
-	@Override
-	public String toString() {
-		return "Worker [id=" + id + ", name=" + name + ", dailyIncome=" + dailyIncome + "]";
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
